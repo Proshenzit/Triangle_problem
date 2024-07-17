e4ba535dfa068fbba3fa99fee53a1b86c188bad0
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-07-17 10:54:47 -0700

    Add files via upload

---
+++ vartika_problem_solve_17.py
@@ -0,0 +1,7 @@
+a = int(input("enter your first angle : "))
+b = int(input("enter your first angle : "))
+c = int(input("enter your first angle : "))
+if a + b + c ==180 and a!=0 and b!=0 and c!=0:
+  print("possible")
+else:
+  print("not pssible")
